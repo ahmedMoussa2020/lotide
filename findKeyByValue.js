03e819c53a0d6284c73514fc038f5caa25a884af
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-11-24 21:50:16 +0000

    found the value

---
+++ findKeyByValue.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (value === object[key]) {
+      return key;
+    }
+  }
+};
+
+module.exports = findKeyByValue;
