19df161e5928ac9c97e117dc80aec76e6242e74b
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-11-24 19:56:35 +0000

    middle array assert

---
+++ middle.js
@@ -0,0 +1,43 @@
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)){
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertEqual = require('./assertEqual');
+const eqArrays1 = function(array1, array2) {
+  if (!(array1 instanceof Array) || !(array2 instanceof Array) || (array1.length !== array2.length)) {
+    return false;
+  } else {
+    let index = array1.length;
+    while (index > 0) {
+      index --;
+      if (array1[index] instanceof Array || array2[index] instanceof Array) {
+        if (!eqArrays(array1[index], array2[index])) {
+          return false;
+        }
+      } else if (array1[index] !== array2[index]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+   
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length) / 2 - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
+  }
+};
+
+module.exports = middle;
