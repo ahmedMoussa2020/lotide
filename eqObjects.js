77f8201867de48dd3a9f119b612c09dcbcf927dd
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-11-24 22:00:46 +0000

    this asignment was done by me

---
+++ eqObjects.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===   ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual}  !== ${expected} `);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let k in object1) {
+      if (object1[k] instanceof Object) {
+        if (!eqObjects(object1[k], object2[k])) {
+          return false;
+        }
+      } else {
+        if (object1[k] !== object2[k]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+
+module.exports = eqObjects;
