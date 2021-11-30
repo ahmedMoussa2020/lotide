fbbe0ec9ba19bc9d0aa5563621aff99960f00677
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2021-11-30 02:18:41 +0000

    all done for the tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,6 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
+//const assertArraysEqual = require('../assertArraysEqual');
 
 /*assertArraysEqual(middle([1, 2, 3]). [2])
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [1])
